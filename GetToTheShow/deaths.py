3817079f93baade9457ad69f891cfb0a74d6ae56
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-03-26 14:41:17 -0400

    bugfix with if/else statements in scenes.py, made the output on the html easier to read, completed a second death scene

--- GetToTheShow/deaths.py
+++ GetToTheShow/deaths.py
@@ -6,12 +6,21 @@ from werkzeug.exceptions import abort
 bp = Blueprint('deaths', __name__)
 
 
-@bp.route('/scenes/sold')
+@bp.route('/scenes/death1')
 def death1():
 
     current_scene = {
         "title": "Wow....I used to think you were cool",
-        "intro": "Why did you even play?\nLame.\nOh well, go back to doing whatever dumb thing you were doing in the first place."
+        "intro": "Why did you even play?\nLame.\n..................................................................................\nOh well, go back to doing whatever dumb thing you were doing in the first place."
     }
 
+    return render_template("death.html", scene=current_scene)
+
+@bp.route('/scenes/death2')
+def death2():
+
+    current_scene = {
+        "title": "",
+        "intro": "COULD ANYONE BLAME YOU?!?!?!\n..................................................................................\nI mean, it\'s not every day you get the opportunity to ask out such an exotic creature. Maybe she\'s a fan of the band, too.\n..................................................................................\nYou sit down beside Katia, and happily accept the drink(s) she starts puttingin front of you.\n..................................................................................\nSuddenly, it all goes black.\n..................................................................................\n\'Why am I so cold?\'... you ask yourself as you slowly come too.\n..................................................................................\nLooking around, the bathroom clearly belongs to one of those crappy motels by the highway.\n..................................................................................\nFreezing, you begin to realize you\'re floating in a tub of ice... \n..................................................................................\n\'Why does my side hurt? Where are my clothes????\'\n..................................................................................\nYou look down, and see the sutures accross your abdomin.....\n..................................................................................\nMY MOTHER F**ING KIDNEY!!!"
+    }
     return render_template("death.html", scene=current_scene)
--- GetToTheShow/scenes.py
+++ GetToTheShow/scenes.py
@@ -15,7 +15,9 @@ def check_valid_answers(action):
     valid_answers = ['Go', 'Sell', 'go', 'sell', 'Ask Kate', 'Ask Katia', 'ask kate',
                      'ask katia', 'ask Kate', 'ask Katia', 'Have a drink', 'Have drink', 'have a drink',
                      'have drink', 'drink', 'Just go', 'just go', 'go', 'Yes', 'yes', 'Say yes', 'say yes',
-                     'do it', 'Do it', 'No', 'no', 'Say no']
+                     'do it', 'Do it', 'No', 'no', 'Say no', 'Say thanks', 'say thanks', 'Thank kate',
+                     'thank kate', 'pretend nothing happened', 'Pretend nothing happened', 'nothing', 'Nothing',
+                     'act like nothing happened', 'Act like nothing happened']
     while True:
         action = request.form['action']
         if action in valid_answers:
@@ -31,64 +33,120 @@ def wintickets():
 
     current_scene = {
         "title": "You FINALLY won tickets!",
-        "intro": "Caller 2,465 ......... the tickets are yours!!!!!!! Finally, having no life is defnately paying off. But the rent is due....... Should you sell, or should you go????"
+        "intro": "\'Caller 2,465 ......... the tickets are YOURS!!!!!!!\'\n..................................................................................\nFinally, having no life is defnately paying off, and countless calls to the local radio station has landed you with 2 tickets to the GREATEST ROCK SHOW EVER, tonight!\n..................................................................................\nBut the rent is due, and missing work \'cause you went too hard at the show ISN\'T a good excuse\n..................................................................................\nShould you sell, or should you go????"
     }
 
     if request.method == 'POST':
         action = request.form['action']
         if check_valid_answers(action) == True:
 
-            if action == "go" or "Go":
+            action = request.form['action']
+
+            if action == "go" or action == "Go":
                 return redirect(url_for('scenes.bar'))
 
-            elif action == "sell" or "Sell":
+            elif action == "sell" or action == "Sell":
                 return redirect(url_for('deaths.death1'))
 
             else:
-                error = "Not the answer we were looking for, try again!"
+                error = "Not the answer we were looking for, try \'Go\' or \'Sell\'!"
         else:
             error = "Not the answer we were looking for, try again!"
 
     return render_template("base.html", scene=current_scene, error=error)
 
 
-@bp.route('/scenes/inline')
+@bp.route('/scenes/inline', methods=('GET', 'POST'))
 def inline():
     tests = ['test', 'also test', 'another test']
 
     current_scene = {
         "title": "Waiting in line outside the show",
-        "intro": "You pull up outside the venue, where the line to get in has wrapped around the block.\nMaking your way to the VIP line, you and Kate cant help but notice all the FREAKS on parade.\nOne dodgy looking dude is making his way up the line.... he looks like Dr. Seuss and Ursula the Sea Witch had a baby.\nHe finally makes it to you... as he stands infront of you, he pulls a small bag from his pocket.......\nLOOKING TO EXPAND YOUR MIND, MAN?????\n He asks, as he waves the baggie.....\nWell, do you??"
+        "intro": "You pull up outside the venue, where the line to get in has wrapped around the block.\n..................................................................................\nMaking your way to the VIP line, you and Kate cant help but notice all the FREAKS on parade.\n..................................................................................\nOne dodgy looking dude is making his way up the line.... he looks like Dr. Seuss and Ursula the Sea Witch had a baby.\n..................................................................................\nHe finally makes it to you... and as he stands infront of you, he pulls a small bag from his pocket.......\n..................................................................................\n\'LOOKING TO EXPAND YOUR MIND, MAN?????\'\n He asks, as he waves the baggie.\n..................................................................................\nWell, are you??"
     }
 
     return render_template("base.html", scene=current_scene)
 
 
-@bp.route('/scenes/bar')
+@bp.route('/scenes/bar', methods=('GET', 'POST'))
 def bar():
     tests = ['test', 'also test', 'another test']
 
     current_scene = {
         "title": "Great, tickets! But you ain't got a ride, man!!",
-        "intro": "You head over to the local watering hole.\nThis place is crowded...... yuck.\nThank GOD Kate\'s working behind the bar.\nShe\'s totally awesome and would LOVE to go rock the f out.... and she drives.\nThe perfect person to ask. She\'ll be getting done soon, too, so thaa----\nWOAH. Wait a minute.\nThere\'s a really hot chick at the back corner table.\nAnd DUDE.... im pretty sure she\'s making eye contact.\n(you look around just to make sure)\nYup. Definately looking at you.\nNow what\'s the play, man?? Ask Kate to go to the concert... or take your chances with \'Katia\'?"
+        "intro": "You head over to the local watering hole.\n..................................................................................\nThis place is crowded. Yuck.\n..................................................................................\nThank GOD Kate\'s working behind the bar. She\'s totally awesome and would LOVE to go rock the F**K out.... and she drives.\n..................................................................................\nThe perfect person to ask. \n..................................................................................\nShe\'ll be getting done soon, too, so thaa----\n..................................................................................\nWOAH. Wait a minute.\n..................................................................................\nThere\'s a really hot chick at the back corner table.\nAnd DUDE.... im pretty sure she\'s making eye contact.\n..................................................................................\n(you look around just to make sure)\n..................................................................................\nYup. Definately looking at you.\n..................................................................................\nNow what\'s the play, man?? Ask Kate to go to the concert... or take your chances with mystery woman \'Katia\'?"
     }
-    # tests=tests)
-    return render_template("base.html", scene=current_scene)
+
+    if request.method == 'POST':
+        action = request.form['action']
+        if check_valid_answers(action) == True:
+
+            if action == "Ask Kate" or action =="ask kate" or action == "Kate" or action == "kate":
+                return redirect(url_for('scenes.havedrink1'))
+
+            elif action == "Ask Katia" or action == "ask katia" or action == "Katia" or action == "katia":
+                return redirect(url_for('deaths.death2'))
+
+            else:
+                error = "Not the answer we were looking for, try \'Ask Kate\' or \'Ask Katia\'!"
+        else:
+            error = "Not the answer we were looking for, try again!"
+
+    return render_template("base.html", scene=current_scene, error=error)
 
 
-@bp.route('/scenes/havedrink')
-def havedrink_part1():
+@bp.route('/scenes/havedrink1', methods=('GET', 'POST'))
+def havedrink1():
     tests = ['test', 'also test', 'anothertest']
 
     current_scene = {
         "title": "",
-        "intro": "Kate\'s definately the wiser choice.....\nMessing with girls like Katia will have you regretting all yur life decisions.\nYou slide up and lay the tickes on the bar, waving Kate over.She looks down and squeals, \'DUDE!!!!!!! F**K YEAH IM DOWN!!!!\nI\'ll be outta here in no time.... you wanna chill and have a drink before we go?\nOr do you just wanna get going right away??\'"
+        "intro": "Kate\'s definately the wiser choice\n..................................................................................\nMessing with girls like Katia will have you regretting all your life decisions.\n..................................................................................\nYou slide up and lay the tickes on the bar, waving Kate over.\n..................................................................................\nShe looks down and squeals, \'DUDE!!!!!!! F**K YEAH IM DOWN!!!!\nI\'ll be outta here in no time.... you wanna chill and have a drink before we go?\nOr do you just wanna get going right away??\'"
     }
+    if request.method == 'POST':
+        action = request.form['action']
+        if check_valid_answers(action) == True:
+
+            if action == "Have a drink" or action == "have a drink" or action == "Drink" or action =="drink":
+                return redirect(url_for('scenes.havedrink2'))
+
+            elif action == "Go" or action == "go" or action == "just go" or action == "Just go":
+                return redirect(url_for('scenes.inline'))
+
+            else:
+                error = "Not the answer we were looking for, try \'Have a drink\' or \'Just go\'"
+        else:
+            error = "Not the answer we were looking for, try again!"
 
     return render_template("base.html", scene=current_scene)  # tests=tests)
 
 
-@bp.route('/scenes/concert')
+@bp.route('/scenes/havedrink2', methods=('GET', 'POST'))
+def havedrink2():
+    tests = ['test', 'also test', 'anothertest']
+
+    current_scene = {
+        "title": "Why not enjoy a drink? You deserve it!",
+        "intro": "You pull up a barstool and wait for Kate to come around.\nThe bartender comes over with a drink,\'Someone bought this for you.\' \n..................................................................................\n\'WELL SHIT, TODAY IS YOUR LUCKY DAY!!!!\n..................................................................................\nWithout even hesitating... you take a huge sip.\n..................................................................................\nCrap.\n..................................................................................\nThe room starts to spin.\nAs the lights begin to fade.... you see a figure approach.\n..................................................................................\n KATIA.\n..................................................................................\n'Goodnight, friend.....\' she whisperes.\n..................................................................................\nYou come too in the front seat of Katie\'s car.\n..................................................................................\n YET AGAIN, she\'s done you a solid.SAVED YOUR LIFE.\n..................................................................................\nShe kicked Katia\'s ass and got you on the road to the concert.\n..................................................................................\nNEVER UNDERESTIMATE A GOOD BARTENDER.\n..................................................................................\nDo you say thanks, or out of embarassment pretend nothing happened?"
+    }
+    if request.method == 'POST':
+        action = request.form['action']
+        if check_valid_answers(action) == True:
+
+            if action == "Say thanks" or action == "say thanks" or action == "Thank kate" or action == "thank kate":
+                return redirect(url_for('scenes.inline'))
+
+            elif action == "pretend nothing happened" or action == "Pretend nothing happened" or action == "nothing" or action == "Nothing" or action == "act like nothing happened" or action == "Act like nothing happened":
+                return redirect(url_for('deaths.death3'))
+
+            else:
+                error = "Not the answer we were looking for, try again!"
+    else:
+        error = "Not the answer we were looking for, try again!"
+
+    return render_template("base.html", scene=current_scene)
+
+
 def concert():
 
     tests = ['test', 'also test', 'another test']
